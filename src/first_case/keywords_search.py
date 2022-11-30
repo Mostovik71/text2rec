543eda98d69c5ff906eb707baac3dd1177f2ba9a
Vsevolod <sesevasa64@gmail.com>
2022-11-30 16:00:23 +0500

    feature: Pipeline() now returns df and filtered query

--- src/first_case/keywords_search.py
+++ src/first_case/keywords_search.py
@@ -18,7 +18,7 @@ class Handler:
     def __init__(self, column_name, value_type=str):
         self.column_name = column_name
         self.value_type = value_type
-    def __call__(self, query: str, df: pd.DataFrame):
+    def __call__(self, df: pd.DataFrame, query: str):
         raise NotImplementedError()
 
 
@@ -30,13 +30,15 @@ class ComplexHandler(Handler):
         self.regex = re.compile(pattern)
         self.regex_with_quotes = re.compile(pattern_with_quotes)
         self.pred = pred
-    def __call__(self, query: str, df: pd.DataFrame):
-        search_result = self.regex.findall(query)
-        search_result_with_quotes = self.regex_with_quotes.findall(query)
-        if not len(search_result) and not len(search_result_with_quotes): 
-            return df
+    def __call__(self, df: pd.DataFrame, query: str):
+        search_match = self.regex.search(query)
+        search_match_with_quotes = self.regex_with_quotes.search(query)
+        if search_match is None and search_match_with_quotes is None: 
+            return df, query
         column = df[self.column_name]
-        first_result: str = search_result[0] if len(search_result) else search_result_with_quotes[0]
+        correct_match = search_match if search_match is not None else search_match_with_quotes
+        filtered_query = query.replace(correct_match.group(), "")
+        first_result: str = correct_match.group(1)
         first_result = first_result.split(',')
         result = pd.Series([True for _ in range(column.size)], index=column.index)
         try:
@@ -44,17 +46,21 @@ class ComplexHandler(Handler):
                 value = self.value_type(value)
                 result &= self.pred(column, value)
         except:
-            return df
-        return df[result]
+            return df, filtered_query
+        return df[result], filtered_query
 
 
 class Pipeline:
     def __init__(self, handlers: List[Handler]):
         self.handlers = handlers
-    def __call__(self, query, df):
+    def __call__(self, df, query):
         for handler in self.handlers:
-            df = handler(query, df)
-        return df
+            df, query = handler(df, query)
+        return df, query
+
+
+def val_in_column(column: pd.Series, value: str):
+    return column.str.contains(value)
 
 
 def main():
@@ -63,21 +69,22 @@ def main():
     args = parser.parse_args()
     query = args.query
 
-    year_handler = ComplexHandler("год", "Year", lambda s, r: s == r, int)
-    director_handler = ComplexHandler("режиссер", "Director", lambda s, r: s.str.contains(r))
-    genre_handler = ComplexHandler("жанр", "Genres", lambda s, r: s.str.contains(r))
-    actor_handler = ComplexHandler("актер", "Actors", lambda s, r: s.str.contains(r))
-    country_handler = ComplexHandler("страна", "Countries", lambda s, r: s.str.contains(r))
-    rating_handler = ComplexHandler("рейтинг", "Rating", lambda s, r: s >= r, float)
+    year_handler     = ComplexHandler("год",      "Year",      lambda c, v: c == v, int)
+    director_handler = ComplexHandler("режиссер", "Director",  val_in_column)
+    genre_handler    = ComplexHandler("жанр",     "Genres",    val_in_column)
+    actor_handler    = ComplexHandler("актер",    "Actors",    val_in_column)
+    country_handler  = ComplexHandler("страна",   "Countries", val_in_column)
+    rating_handler   = ComplexHandler("рейтинг",  "Rating",    lambda c, v: c >= v, float)
 
     pipeline = Pipeline([
         rating_handler, year_handler, director_handler, 
         genre_handler, actor_handler, country_handler
     ])
 
-    df = pd.read_csv("data/top250.csv")
-    df = pipeline(query, df)
+    df = pd.read_csv("top250.csv")
+    df, query = pipeline(df, query)
     print(df)
+    print(query)
 
 
 if __name__ == "__main__":
