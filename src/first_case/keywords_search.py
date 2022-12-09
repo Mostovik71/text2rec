2e5f7d0d759fd1a8c5f60a2b8f7c88eb67fba4a4
Vsevolod <sesevasa64@gmail.com>
2022-12-09 17:35:59 +0500

    added new notebooks

--- src/first_case/keywords_search.py
+++ src/first_case/keywords_search.py
@@ -9,9 +9,30 @@ def regex_for_query_without_quotes(name, suffix = "ы"):
     return f"{name}{suffix}:([\w,.]+)"
 
 
-def regex_for_query_with_quotes(name, suffix = "ы"):
+def regex_for_query_with_quotes(name, quote_type="'", suffix = "ы"):
     if suffix != "": suffix += "?"
-    return f"{name}{suffix}:'([\w,. ]+)'"
+    return f"{name}{suffix}:{quote_type}([\w,. ]+){quote_type}"
+
+
+def val_in_column(column: pd.Series, value: str):
+    return column.str.contains(value)
+
+
+class TagSeach:
+    def __init__(self, tag_name, tag_suffix="ы"):
+        single_value_pattern = regex_for_query_without_quotes(tag_name, tag_suffix)
+        multiple_values_patterns = [
+            regex_for_query_with_quotes(tag_name, quote_type=qt, suffix=tag_suffix) 
+            for qt in ["'", "\""]
+        ]
+        self.patterns = [single_value_pattern] + multiple_values_patterns
+        self.regexps = [re.compile(p) for p in self.patterns]
+    def search(self, query) -> re.Match:
+        for regexp in self.regexps:
+            match = regexp.search(query)
+            if match:
+                return match
+        return None
 
 
 class Handler:
@@ -23,20 +44,15 @@ class Handler:
 
 
 class ComplexHandler(Handler):
-    def __init__(self, field_name, column_name, pred, cast_to_type=str, **kwargs):
+    def __init__(self, field_name, column_name, pred=val_in_column, cast_to_type=str, **kwargs):
         super().__init__(column_name, cast_to_type)
-        pattern = regex_for_query_without_quotes(field_name, **kwargs)
-        pattern_with_quotes = regex_for_query_with_quotes(field_name, **kwargs)
-        self.regex = re.compile(pattern)
-        self.regex_with_quotes = re.compile(pattern_with_quotes)
+        self.tag_search = TagSeach(field_name, **kwargs)
         self.pred = pred
     def __call__(self, df: pd.DataFrame, query: str):
-        search_match = self.regex.search(query)
-        search_match_with_quotes = self.regex_with_quotes.search(query)
-        if search_match is None and search_match_with_quotes is None: 
+        correct_match = self.tag_search.search(query)
+        if correct_match is None:
             return df, query
         column = df[self.column_name]
-        correct_match = search_match if search_match is not None else search_match_with_quotes
         filtered_query = query.replace(correct_match.group(), "")
         first_result: str = correct_match.group(1)
         first_result = first_result.split(',')
@@ -59,22 +75,18 @@ class Pipeline:
         return df, query
 
 
-def val_in_column(column: pd.Series, value: str):
-    return column.str.contains(value)
-
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('query', metavar="query")
     args = parser.parse_args()
     query = args.query
 
-    year_handler     = ComplexHandler("год",      "Year",      lambda c, v: c == v, int)
-    director_handler = ComplexHandler("режиссер", "Director",  val_in_column)
-    genre_handler    = ComplexHandler("жанр",     "Genres",    val_in_column)
-    actor_handler    = ComplexHandler("актер",    "Actors",    val_in_column)
-    country_handler  = ComplexHandler("страна",   "Countries", val_in_column)
-    rating_handler   = ComplexHandler("рейтинг",  "Rating",    lambda c, v: c >= v, float)
+    year_handler = ComplexHandler("год", "Year", lambda c, v: c == v, int)
+    rating_handler = ComplexHandler("рейтинг", "Rating", lambda c, v: c >= v, float)
+    director_handler = ComplexHandler("режиссер", "Director")
+    genre_handler = ComplexHandler("жанр", "Genres")
+    actor_handler = ComplexHandler("актер", "Actors")
+    country_handler = ComplexHandler("страна", "Countries")
 
     pipeline = Pipeline([
         rating_handler, year_handler, director_handler, 
